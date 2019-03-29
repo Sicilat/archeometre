6231d3554058ea465ceb9392991016fc22fbaedc
Sicilat <sicilat@gmail.com>
2019-03-29 09:39:54 +0100

    Arduino machin

--- porg.ino
+++ porg.ino
@@ -1,29 +1,47 @@
 //Initialisation des tableaux
 
-double us;
-double pos[3];
 double cap[3];
 
+const float SOUND_SPEED = 340.0 / 1000;           /* Vitesse du son dans l'air en mm/us */
+const byte TRIGGER_PIN = 2;                       // Broche TRIGGER
+const byte ECHO_PIN = 4;                          // Broche ECHO
+const unsigned long MEASURE_TIMEOUT = 25000UL;    // 25ms = ~8m à 340m/s
+const int potA = 5;  //Défini le pin du potentiometre
+const int potB = 4;
+const int potC = 3;
+const int btn = 5
+const int L1 = 3;
+
 double getUSvalue(){
+	digitalWrite(TRIGGER_PIN, HIGH);
+  	delayMicroseconds(10);
+  	igitalWrite(TRIGGER_PIN, LOW);
+
+  	long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  	float distance_mm = measure / 2.0 * SOUND_SPEED;
+
+  	float data = distance_mm / 1000.0, 2;
 	return data;
 }
 
-void transmit_data(double cap[], double pos[], double us){
-	//Transmettre les données à l'interface intermédiaire	
+void transmit_data(double cap[], double us){
+	//Transmettre les données à l'interface
 }
 
 double getRAcap(){
-	//Récupérer la valeur du capteur 1
+	int data = analogRead(potA);
 	return data;
 }
 
 double getRBcap(){
 	//Récupérer la valeur du capteur 2
+	int data = analogRead(potB);
 	return data;
 }
 
 double getRCcap(){
 	//Récupérer la valeur du capteur 3
+	int data = analogRead(potC);
 	return data;
 }
 
@@ -35,7 +53,7 @@ double get_cap(double cap[]){
 }
 
 void setup(){
-	Serial.begin(115200);
+	Serial.begin(9600);
 	
 	//Initialisation des capteurs US
 
@@ -46,8 +64,19 @@ void setup(){
 	cap[0] = 0;
 	cap[1] = 0;
 	cap[2] = 0;
+
+  	pinMode(TRIGGER_PIN, OUTPUT);   //initialise les broches.
+  	digitalWrite(TRIGGER_PIN, LOW);
+  	pinMode(ECHO_PIN, INPUT);
+  	pinMode(L1, OUTPUT); //L1 est une broche de sortie
+  	pinMode(potA, INPUT);
+  	pinMode(potB, INPUT);
+  	pinMode(potC, INPUT);
 }
 
 void loop(){
-	
+	digitalWrite(L1, HIGH);
+	if digitalRead(btn){
+		transmit_data(get_cap(), getUSvalue())
+	}
 }
