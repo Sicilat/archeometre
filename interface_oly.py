9aa571ccb2bba9ec7b955ee1ee8d3881d9c55f06
Sicilat <44788236+Sicilat@users.noreply.github.com>
2019-04-14 18:51:45 +0200

    Oly files for wednesday

---
+++ interface_oly.py
@@ -0,0 +1,67 @@
+import serial, time, os
+from math import *
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+print(ser.name)
+refs = [0, 0]
+
+def get_refs():
+	data = [0, 0]
+	f = open('./ref_oly.txt', 'r', encoding = "UTF-8")
+	lines = f.readlines()
+	data[0] = int(lines[0].rstrip())
+	data[1] = int(lines[1].rstrip())
+	f.close()
+	return data
+
+def clear():
+	os.system('clear||cls')
+
+def write_file(data):
+	f = open('./output_oly.txt', 'a', encoding = "UTF-8")
+	f.write('\nCap_A = ' + str(data[0]) + '\r\n')
+	f.write('US =    ' + str(data[1]) + '\r\n')
+	f.write('Ref_X = ' + str(data[4][0]) + '\r\n')
+	f.write('Ref_Z = ' + str(data[4][1]) + '\r\n')
+	f.close()
+	return
+
+def confirm_data(data):
+	clear()
+	print('Received information !')
+	print('Cap_A = ' + str(data[0]))
+	print('US =    ' + str(data[1]))
+	print('Ref_X = ' + str(data[2][0]))
+	print('Ref_Z = ' + str(data[2][1]))
+	print('Do you want to confirm these values ? (y/n)')
+	asw = 'o'
+	while asw != 'y' and asw != 'n':
+		asw = input('> ').lower()
+	if asw == 'y':
+		write_file(data)
+		return
+	else:
+		return
+
+def parse_lenght(data):
+	return (data / 1024) * 1.39
+
+def handle_data(ser, asw, refs):
+	if asw == 'trsm':
+		data = [0, 0, [0, 0]]
+		data[0] = parse_lenght(float(ser.readline().rstrip().decode()))
+		data[1] = float(ser.readline().rstrip().decode())
+		refs = get_refs()
+		data[2][0], data[2][1] = refs
+		confirm_data(data)
+	return
+
+while True:
+	s = ser.read(10)
+	line = ser.readline().rstrip().decode()
+	print(line)
+	if line != '':
+		handle_data(ser, line, refs)
+	ser.flushInput()
+	ser.flushOutput()
+	time.sleep(0.5)
---
+++ porg_oly.ino
@@ -0,0 +1,71 @@
+double cap;
+
+const float SOUND_SPEED = 340.0 / 1000;           /* Vitesse du son dans l'air en mm/us */
+const byte TRIGGER_PIN = 2;                       // Broche TRIGGER
+const byte ECHO1_PIN = 4;
+const unsigned long MEASURE_TIMEOUT = 25000UL;    // 25ms = ~8m à 340m/s
+const int potA = 5;
+const int btn = 7;
+const int L1 = 3;
+double distance_mm;
+
+double getUSvalue(float distance_mm){
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER_PIN, LOW);
+    double distance_mm1;
+    
+    long measure1 = pulseIn(ECHO1_PIN, HIGH, MEASURE_TIMEOUT);
+    distance_mm1 = measure1 / 2.0 * SOUND_SPEED / 1000;
+
+    float data = distance_mm1;
+  return data;
+}
+
+void transmit_data(double cap[], double us){
+  Serial.println("trsm");
+  Serial.println(cap);
+  Serial.println(us);
+}
+
+double getRAcap(){
+  int a = 0;
+  for (int i = 0; i < 1000; i++){
+    int data = analogRead(potA);
+    a++;
+  }
+  data = data / a;
+  return data;
+}
+
+int get_cap(double cap){
+  cap = getRAcap();
+  return cap;
+}
+
+void setup(){
+  Serial.begin(9600);
+  
+  //Initialisation des capteurs US
+
+  double us = 0;
+  
+  //Initialisation du tableau des capteurs fils tendus
+  
+  cap = 0;
+
+  pinMode(TRIGGER_PIN, OUTPUT);   //initialise les broches.
+  digitalWrite(TRIGGER_PIN, LOW);
+  pinMode(ECHO1_PIN, INPUT);
+  pinMode(L1, OUTPUT);
+  pinMode(potA, INPUT);
+  pinMode(btn, INPUT);
+}
+
+void loop(){
+  digitalWrite(L1, HIGH);
+  if (digitalRead(btn) == 0){
+    transmit_data(get_cap(cap), getUSvalue(distance_mm));
+    delay(100);
+  }
+}
